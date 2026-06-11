4a598d0de7e0afd523eb1c93fb40707b67a95bfa
annisafathul <annisafathul18@gmail.com>
2026-06-11 15:58:36 +0700

    menambahkan logika insert untuk root

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -39,8 +39,21 @@ class BinaryTree
       Node *currentNode = NULL;
 
       search(element, parent, currentNode);
+      if (parent == NULL)
+      {
+        ROOT = newNode;
+        return;
 
+      }
 
+      if (element < parent->info)
+      {
+        parent->leftchild = newNode;
+      }
+      else if (element > parent->info)
+      {
+        parent->rightchild = newNode;
+      }
 
-
-};
+    }
+}
